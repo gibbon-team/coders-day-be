f73a2eace8f6ee1becd96bef43d7722a1c77b3b2
Paweł Siwoń <pawel.siwon@ttpsc.pl>
2020-06-18 21:17:27 +0200

    redo HelloWorld class test

--- src/test/java/com/codersday/HelloWorldTest.java
+++ src/test/java/com/codersday/HelloWorldTest.java
@@ -1,30 +1,13 @@
 package com.codersday;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+@SpringBootTest
+class HelloWorldTest {
 
-public class HelloWorldTest {
-
-    public void test_getHelloWorld_equals() {
-        // given
-        String expected = "HelloWorld";
-
-        // when
-        String result = HelloWorld.getHelloWorld();
-
-        // then
-        assertEquals(expected, result);
+    @Test
+    void contextLoads() {
     }
 
-    public void test_getHelloWorld_notEquals() {
-        // given
-        String notExpected = "GoodbyeWorld";
-
-        // when
-        String result = HelloWorld.getHelloWorld();
-
-        // then
-        assertNotEquals(notExpected, result);
-    }
 }
