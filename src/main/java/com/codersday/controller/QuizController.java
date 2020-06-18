574947bf4acbd00e4054c6b783c086d1c171e3d4
Paweł Siwoń <pawel.siwon@ttpsc.pl>
2020-06-19 00:14:53 +0200

    null pointer exeception

--- src/main/java/com/codersday/controller/QuizController.java
+++ src/main/java/com/codersday/controller/QuizController.java
@@ -2,9 +2,14 @@ package com.codersday.controller;
 
 import java.util.List;
 
+import com.codersday.model.Answer;
+import com.codersday.model.Question;
 import com.codersday.model.Quiz;
+import com.codersday.repository.AnswerRespository;
+import com.codersday.repository.QuestionRepository;
 import com.codersday.repository.QuizRepository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/quiz")
+@RequiredArgsConstructor
 public class QuizController {
 
-    QuizRepository quizRepository;
-
-    public QuizController(@Autowired QuizRepository quizRepository) {
-        this.quizRepository = quizRepository;
-    }
+    private final QuizRepository quizRepository;
+    private final AnswerRespository answerRespository;
+    private final QuestionRepository questionRepository;
 
     @PostMapping
     public ResponseEntity<Quiz> saveQuiz(@RequestBody Quiz quiz) {
-        quiz = quizRepository.save(quiz);
-        return ResponseEntity.ok(quiz);
+        quiz.getQuestions().forEach(question -> {
+            List<Answer> answers1 = answerRespository.saveAll(question.getAnswers());
+            question.setAnswers(answers1);
+        });
+
+        List<Question> questions = questionRepository.saveAll(quiz.getQuestions());
+        quiz.setQuestions(questions);
+
+        Quiz saved = quizRepository.save(quiz);
+
+        return ResponseEntity.ok(saved);
     }
 
     @GetMapping
