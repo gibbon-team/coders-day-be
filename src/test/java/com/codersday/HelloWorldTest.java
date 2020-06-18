e6238f209f6c3aa4fc6cc39f11daea96527f2610
Artur Węgrzyn <Artur.Wegrzyn@ttpsc.pl>
2020-06-18 15:01:50 +0200

    add initial files, jenkins, maven helloworld

--- src/test/java/com/codersday/HelloWorldTest.java
+++ src/test/java/com/codersday/HelloWorldTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.codersday;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class HelloWorldTest {
-  
+
+    void test_getHelloWorld_equals() {
+        // given
+        String expected = "HelloWorld";
+
+        // when
+        String result = HelloWorld.getHelloWorld();
+
+        // then
+        assertEquals(expected, result);
+    }
+
+    void test_getHelloWorld_notEquals() {
+        // given
+        String notExpected = "GoodbyeWorld";
+
+        // when
+        String result = HelloWorld.getHelloWorld();
+
+        // then
+        assertNotEquals(notExpected, result);
+    }
 }
